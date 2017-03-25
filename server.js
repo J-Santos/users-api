59d9369739cae48c78953f20c49aa46eabb69750
Jelson Santos <jesantos0527@gmail.com>
2017-03-25 14:40:30 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express     =   require("express");
+var app         =   express();
+var bodyParser  =   require("body-parser");
+var mongoOp     =   require("./models/users");
+var router      =   express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended" : false}));
+
+router.get("/",function(req,res){
+    res.json({"error" : false,"message" : "Hello World"});
+});
+
+
+router.route("/users")
+    .get(function(req,res){
+        var response = {};
+        mongoOp.find({},function(err,data){
+        // Mongo command to fetch all data from collection.
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                response = {"error" : false,"message" : data};
+            }
+            res.json(response);
+        });
+    });
+
+app.use('/',router);
+
+app.listen(3000);
+console.log("Listening to PORT 3000");
